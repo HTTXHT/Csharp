9e8b4434ed4a0ceba46fc948702498203101846e
HTTXHT <httxht@qq.com>
2025-02-21 16:15:16 +0800

    在方法间值的传递和接收

--- helloCS/helloCS/Program.cs
+++ helloCS/helloCS/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Permissions;
+using System.Xml.Linq;
 
 namespace helloCS     //命名空间
 {
@@ -9,12 +10,12 @@ namespace helloCS     //命名空间
         
         static void Main()    //函数，方法
         {
+
             GetUserInfo();
 
 
 
             Console.ReadKey();
-
         }
 
 
@@ -27,6 +28,31 @@ namespace helloCS     //命名空间
             string name = Console.ReadLine();
             Console.WriteLine();
 
+            //家乡收集
+            Console.WriteLine("请输入你的故乡");
+            string address = Console.ReadLine();
+            Console.WriteLine();
+
+            //收集爱好
+            Console.WriteLine("请输入你的爱好");
+            string aihao = Console.ReadLine();
+            Console.WriteLine();
+
+
+            name = ChangeData(name);
+
+
+            //按格式输出
+            Console.WriteLine("你的名字是:" + name);
+            Console.WriteLine("你的故乡是:" + address);
+            Console.WriteLine("你的爱好是:" + aihao);
+        }
+
+
+
+
+        static string  ChangeData(string name)
+        {
             if (name == "张三")
             {
                 Console.WriteLine("你输入的是张三");
@@ -43,25 +69,8 @@ namespace helloCS     //命名空间
             }
             Console.WriteLine();
 
-
-            //家乡收集
-            Console.WriteLine("请输入你的故乡");
-            string address = Console.ReadLine();
-            Console.WriteLine();
-
-            //收集爱好
-            Console.WriteLine("请输入你的爱好");
-            string aihao = Console.ReadLine();
-            Console.WriteLine();
-
-            //按格式输出
-            Console.WriteLine("你的名字是:" + name);
-            Console.WriteLine("你的故乡是:" + address);
-            Console.WriteLine("你的爱好是:" + aihao);
+            return name;
         }
-
-
-
 
 
     }
