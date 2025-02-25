970081bbbd574726242cf171d5ae3ecc816c2df6
HTTXHT <httxht@qq.com>
2025-02-25 15:18:01 +0800

    优化角色面板ui

--- helloCS/helloCS/Program.cs
+++ helloCS/helloCS/Program.cs
@@ -7,6 +7,16 @@ namespace wodekongjian     //命名空间
     {
         static void Main()    //函数，方法
         {
+            Background();
+
+            CharacterCreation();
+
+        }
+
+
+        //故事背景
+        static void Background()
+        {
             Console.WriteLine("月黑风高杀人夜");
             Console.WriteLine("按任意键继续……");
             Console.ReadKey();
@@ -21,8 +31,13 @@ namespace wodekongjian     //命名空间
             Console.WriteLine("按任意键继续……");
             Console.ReadKey();
             Console.WriteLine();
+        }
 
-            while (true) 
+
+        //创建角色
+        static void CharacterCreation()
+        {
+            while (true)
             {
                 Console.WriteLine("请输入性别：");
                 string xingbie = Console.ReadLine();
@@ -51,46 +66,37 @@ namespace wodekongjian     //命名空间
                 }
             }
         }
-           
 
     }
 
 
+//角色类
     class Hero
     {
+        //角色属性
         public string mingzi;
         public int nianling;
         public int shenggao;
         public int gongji;
         public int xueliang;
+        public int jineng;
         public string jineng1;
         public int jineng1gongji;
         public string jineng2;
         public int jineng2gongji;
 
-
+        //生成属性值
         public void ShowGeneratingShux(string xingbie)
         {
-            Console.WriteLine("请输入名称：");
-            mingzi = Console.ReadLine();
-
-            while (string.IsNullOrWhiteSpace(mingzi))
-            {
-                Console.WriteLine("请正确输入名称：");
-                mingzi = Console.ReadLine();
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("原来你就是传说中的" + mingzi + "大侠！");
-
+            MingZi();
             if (xingbie == "男")
             {
- 
                 xingbie = "男";
                 nianling = 18;
                 shenggao = 175;
                 gongji = 10;
                 xueliang = 100;
+                jineng = 2;
                 jineng1 = "飞沙走石";
                 jineng1gongji = 20;
                 jineng2 = "千里冰封";
@@ -104,46 +110,64 @@ namespace wodekongjian     //命名空间
                 shenggao = 165;
                 gongji = 10;
                 xueliang = 100;
+                jineng = 2;
                 jineng1 = "天山折梅手";
                 jineng1gongji = 30;
                 jineng2 = "九阴白骨爪";
                 jineng2gongji = 70;
-                ShowInfo( xingbie);
+                ShowInfo(xingbie);
             }
-            
+
         }
 
+        //注册
+        public void MingZi()
+        {
+
+            Console.WriteLine("请输入名称：");
+            mingzi = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(mingzi))
+            {
+                Console.WriteLine("请正确输入名称：");
+                mingzi = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("原来你就是传说中的" + mingzi + "大侠！");
+        }
+
+
+        //查看个人面板
         public void ShowInfo(string xingbie)
         {
 
-            Console.WriteLine("按任意键查看属性");
+            Console.WriteLine("按任意键查看个人面板");
             Console.ReadKey();
             Console.WriteLine();
-
-            Console.WriteLine("────────────────────────────");
-            Console.WriteLine("         属性             ");
-            Console.WriteLine("────────────────────────────");
-            Console.WriteLine($" 姓名:       {mingzi}");
+            Console.WriteLine("╔══════════════════════════╗");
+            Console.WriteLine("║           属性           ║");
+            Console.WriteLine("╚══════════════════════════╝");
+            Console.WriteLine($" 大侠:       {mingzi}");
             Console.WriteLine($" 性别:       {xingbie}");
             Console.WriteLine($" 年龄:       {nianling}");
             Console.WriteLine($" 身高:       {shenggao}");
-            Console.WriteLine($" 攻击力:     {gongji}");
+            Console.WriteLine($" 攻击:     {gongji}");
             Console.WriteLine($" 血量:       {xueliang}");
-            Console.WriteLine("────────────────────────────");
-            Console.WriteLine("         技能             ");
-            Console.WriteLine("────────────────────────────");
+            Console.WriteLine($" 技能数量:   {jineng}");
+            Console.WriteLine("╔══════════════════════════╗");
+            Console.WriteLine("║         技能展示         ║");
+            Console.WriteLine("╚══════════════════════════╝");
             Console.WriteLine($" 技能1:      {jineng1}");
-            Console.WriteLine($" 伤害: {jineng1gongji}");
+            Console.WriteLine($" 伤害:       {jineng1gongji}");
             Console.WriteLine($" 技能2:      {jineng2}");
-            Console.WriteLine($" 伤害: {jineng2gongji}");
-            Console.WriteLine("────────────────────────────");
-
-            Console.WriteLine("按任意键继续……");
-            Console.ReadKey();
+            Console.WriteLine($" 伤害:       {jineng2gongji}");
+            //Console.WriteLine("═══════════════════════════");
             Console.WriteLine();
+            Console.WriteLine("按任意键继续……");
+            Console.WriteLine();
+            Console.ReadKey();
         }
-
-
     }
 
 
