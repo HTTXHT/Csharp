5c809190919e390cb78ef8ee71e17ed3cd023223
HTTXHT <httxht@qq.com>
2025-03-18 17:03:37 +0800

    更新RazorPagesMovie

--- RazorPagesMovie/RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf5ac552ac26b74bed459463af5a0828e8965cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126a0d9ad134592c442ca19f1c61b9a72f02e8e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
